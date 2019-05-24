790c321bbdfb6dd8124295b8583cc80de5892034
Austin Born <austinborn212@gmail.com>
2019-05-24 15:06:33 -0500

    Finished parsing OHLC historical data for testing

--- historicalTesting.py
+++ historicalTesting.py
@@ -1,8 +1,4 @@
 '''
-Test core AI with historical data from .csv files
-'''
-
-"""
 Handle .CSV files for 1-min historical data in the following manner:
     Given:
     - Historical range of data to test (start, end date)
@@ -12,15 +8,24 @@ Handle .CSV files for 1-min historical data in the following manner:
     Return:
     - Change in investments at each time step
     - Net change of portfolio value
+'''
+
+"""
+TODO
+- Refactor/simplify code
+- Check date travelling bugs
+- Check usd conversion bugs
+
 """
 import csv
+from coreAI import coreAI
 
 def run_data(startDate, endDate, symbols, start_investments):
 
     candle_ohlc_data = {} # Form: {"SYMBOL": [open, high, low, close]}
     market_files = {} # Form: {"SYMBOL": file}
     file_readers = {} # Form: {"SYMBOL": reader}
-    cur_investments = start_investments
+    cur_investments = start_investments # Form: {"SYMBOL": amount}
     startYear = startDate.split(' ')[0].split('-')[0]
 
     # For each symbol, open first file
@@ -32,22 +37,24 @@ def run_data(startDate, endDate, symbols, start_investments):
         try:
             file = open(path_to_file, 'r')
         except FileNotFoundError:
-            print("Error: could not open historical data for "+symbol)
-            return cur_investments
+            print("Error: could not open historical data for "+symbol+" in "+startYear)
+            return start_investments
 
         market_files[symbol] = file
-        file_readers[symbol] = csv.reader(file, delimiter=',')
+        file_readers[symbol] = reversed(list(csv.reader(file, delimiter=',')))
       
     # Get each reader to start date
     for symbol in symbols:
         reader = file_readers[symbol]
-        title = next(reader)
-        headers = next(reader)
         row = next(reader)
-        print(row)
-        print(startDate)
-        while(row[1] != startDate):
+        i = 0
+        oldrow = row
+        while(row[1] < startDate):
+            oldrow = row
             row = next(reader)
+        if row[1] != startDate:
+            print("Error: "+symbol+" dataset jumps from "+str(oldrow[1])+" to "+str(row[1]))
+            return start_investments
         candle_ohlc_data[symbol] = row[3:6]
 
     # Call AI with first Candle data
@@ -56,10 +63,10 @@ def run_data(startDate, endDate, symbols, start_investments):
     # Continue through dates
     curDate = startDate
     curYear = startYear
-    while curDate != endDate:
+    while curDate < endDate:
 
-        if curDate == '12/31/'+curYear+' 23:59':
-            curYear += 1
+        if curDate == curYear+'-12-31 23:59:00':
+            curYear = str(int(curYear) + 1)
             
             for symbol in symbols:
                 # Close this file, try to open new file
@@ -70,13 +77,11 @@ def run_data(startDate, endDate, symbols, start_investments):
                 try:
                     file = open(path_to_file, 'r')
                 except FileNotFoundError:
-                    print("Error: could not open historical data for "+symbol)
-                    return cur_investments
+                    print("Error: could not open historical data for "+symbol+" in "+startYear)
+                    return start_investments
 
                 market_files[symbol] = file
-                file_readers[symbol] = csv.reader(file, delimiter=',')
-                title = next(reader)
-                headers = next(reader)
+                file_readers[symbol] = reversed(list(csv.reader(file, delimiter=',')))
             
         curDateSet = False 
         for symbol in symbols:
@@ -91,17 +96,41 @@ def run_data(startDate, endDate, symbols, start_investments):
 
         # Call AI and update investments
         cur_investments = coreAI(cur_investments, candle_ohlc_data)
-    
+
+    if curDate != endDate:
+        print("Error: dataset jumps to "+curDate)
+        return start_investments
+    for symbol in symbols:
+        market_files[symbol].close()
 
     return cur_investments
 
 # Calculate net worth from investment volumes
 def net_worth(date, investments):
     net_worth = 0.0
+    year = date.split(' ')[0].split('-')[0]
 
     for coin in investments:
-        # Find current value of coin using close value on date
-        usd_conversion = 0
+        path_to_file = './Gemini_Data/'+coin+'USD/gemini_'+coin+'USD_'+year+'_1min.csv'
+
+        try:
+            file = open(path_to_file, 'r')
+        except FileNotFoundError:
+            print("Error: could not open historical data for "+coin+"USD in "+year)
+            return investments
+
+        reader = reversed(list(csv.reader(file, delimiter=',')))
+        row = next(reader)
+
+        while(row[1] < date):
+            oldrow = row
+            row = next(reader)
+        if row[1] != date:
+            print("Error: "+coin+"USD dataset jumps from "+str(oldrow[1])+" to "+str(row[1]))
+            return investments
+        
+        # USD = close value at date
+        usd_conversion = float(row[6])
 
         # Multiply volume by usd conversion
         net_worth += investments[coin] * usd_conversion
@@ -112,18 +141,18 @@ def net_worth(date, investments):
 if __name__ == "__main__":
 
     # Start and end times
-    startDate = "2018-12-7 9:12:00"
-    endDate = "2018-12-7 9:13:00"
+    startDate = "2018-12-07 21:21:00"
+    endDate = "2018-12-08 21:23:00"
 
     # Symbols in workspace
     symbols = ['BTCUSD', 'ETHUSD']
 
     # Current investments
     start_investments = {"BTC": 1.0, "ETH": 0.5}
-    print("Start net worth: "+str(net_worth(startDate, start_investments)))
+    print("Start net worth on "+startDate+": "+str(net_worth(startDate, start_investments)))
 
     # End investments
     end_investments = run_data(startDate, endDate, symbols, start_investments)
-    print("Start net worth: "+str(net_worth(endDate, end_investments)))
+    print("End net worth on "+startDate+": "+str(net_worth(endDate, end_investments)))
 
     
