026e67395db458d59ec1a140f18cd477ebdf7ace
Austin Born <austinborn212@gmail.com>
2019-05-30 23:31:19 -0500

    Adding comments on indicators

--- indicators.py
+++ indicators.py
@@ -15,10 +15,19 @@ Assume that the indicators have access to:
 - OHLC (open-high-low-close) array
 '''
 
-def sma(vwap_array, ohlc_array):
+# Depends only on previous sma and current vwap
+def sma(vwap_array):
+    
+
+    # Array checking
+    if len(vwap_array) < array_min:
+        return
+
+    
 
     return
 
+# Depends only on previous ema and current vwap
 def ema(vwap_array, ohlc_array):
 
     return
